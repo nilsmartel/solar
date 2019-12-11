24b2799f897b917998017278ae1e9922de4a8e62
Nils Martel <nils.martel@graphmasters.net>
2019-12-11 12:59:09 +0100

    Implement Parse for Variable Declarations

--- src/parse/instructions/expression.rs
+++ src/parse/instructions/expression.rs
@@ -1,2 +1,9 @@
+use crate::parse::Parse;
 #[derive(Clone, Debug)]
 pub struct Expression {}
+
+impl Parse for Expression {
+    fn parse(input: &str) -> nom::IResult<&str, Self> {
+        unimplemented!()
+    }
+}
--- src/parse/signature/vars.rs
+++ src/parse/signature/vars.rs
@@ -1,7 +1,35 @@
-use crate::parse::{instructions::Expression, signature::Type, terminals::Identifier};
+use crate::parse::{instructions::Expression, signature::Type, terminals::Identifier, Parse};
 #[derive(Clone, Debug)]
 pub struct VariableDecl {
     name: Identifier,
     kind: Type,
     assign: Option<Box<Expression>>,
+}
+
+impl Parse for VariableDecl {
+    fn parse(input: &str) -> nom::IResult<&str, Self> {
+        use nom::{
+            bytes::complete::tag,
+            character::complete::one_of,
+            combinator::{map, opt},
+            sequence::{preceded, terminated, tuple},
+        };
+
+        use crate::parse::util::char_ws;
+
+        map(
+            tuple((
+                preceded(
+                    terminated(tag("let"), one_of(" \n\t\r")),
+                    Identifier::parse_ws,
+                ),
+                preceded(char_ws(':'), Type::parse_ws),
+                opt(preceded(
+                    char_ws('='),
+                    map(Expression::parse_ws, |e| Box::new(e)),
+                )),
+            )),
+            |(name, kind, assign)| VariableDecl { name, kind, assign },
+        )(input)
+    }
 }
