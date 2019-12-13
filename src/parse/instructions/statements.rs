7d8dd2276f1e798abbeedcdf78e29a80644f94a2
Nils Martel <nils.martel@graphmasters.net>
2019-12-13 13:32:11 +0100

    Parsing of assignment

--- src/parse/instructions/statements.rs
+++ src/parse/instructions/statements.rs
@@ -1,18 +1,56 @@
 use crate::parse::{
     instructions::{ControlFlow, Expression},
+    keyword::tag_do,
     terminals::Identifier,
+    Parse,
 };
 
 /// do abc = <expr>;
 #[derive(Clone, Debug)]
 pub struct AssignStatement {
     variable: Identifier,
+    kind: AssignmentType,
     assign: Box<Expression>,
 }
 
-#[derive(Copy, Clone)]
+impl Parse for AssignStatement {
+    fn parse(input: &str) -> nom::IResult<&str, Self> {
+        use nom::combinator::map;
+        use nom::sequence::preceded;
+        use nom::sequence::tuple;
+
+        map(
+            tuple((
+                preceded(tag_do, Identifier::parse_ws),
+                AssignmentType::parse_ws,
+                map(Expression::parse_ws, Box::new),
+            )),
+            |(variable, kind, assign)| AssignStatement {
+                variable,
+                kind,
+                assign,
+            },
+        )(input)
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
 pub enum AssignmentType {
     Assign,
+    Add,
+    Subtract,
+}
+
+impl Parse for AssignmentType {
+    fn parse(input: &str) -> nom::IResult<&str, Self> {
+        use nom::{branch::alt, bytes::complete::tag, combinator::map};
+
+        alt((
+            map(tag("="), |_| AssignmentType::Assign),
+            map(tag("+="), |_| AssignmentType::Add),
+            map(tag("-="), |_| AssignmentType::Subtract),
+        ))(input)
+    }
 }
 
 #[derive(Clone, Debug)]
--- src/parse/keyword.rs
+++ src/parse/keyword.rs
@@ -4,3 +4,12 @@ pub fn is_keyword(s: &str) -> bool {
         _ => false,
     }
 }
+
+use crate::parse::util::*;
+
+pub fn tag_do(i: &str) -> nom::IResult<&str, &str> {
+    not_followed(
+        nom::bytes::complete::tag("do"),
+        nom::character::complete::one_of(" \n\r\t"),
+    )(i)
+}
